9b0fc9caf5259d4e5e3d7e75578e187ce241800d
andressa-mb <andressamesquita26@gmail.com>
2025-09-01 15:12:41 -0300

    Projeto base

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row mx-2 my-3">
+        <div class="col-md-12 d-flex justify-content-between">
+            <div class="top-left">
+                <h2>Nutrição</h2>
+            </div>
+        </div>
+
+    </div>
+    <div class="row mx-2 my-3">
+        <div class="col-md-12">
+            <p>Conteúdo</p>
+        </div>
+    </div>
+@endsection
